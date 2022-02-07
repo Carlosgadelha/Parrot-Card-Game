cd02a769ad63d66db74535eaf988365c79944935
Carlos Gadelha <josecarlos.gadelha10@gmail.com>
2022-02-06 21:35:08 -0300

    corrigindo bug entrada invalida, na opção de reiniciar o jogo e adicionando os atributos para os testes automatizados

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const animais = ["Leão","girafa","elefante","zebra","hipopotamo","tigre","macaco"];
-let cardAtual, cardAnterior, controlador, carta01, carta02, cartas, cards, entrada, acertos, quantidadeJogadas, tempo, segundo
+let cardAtual, cardAnterior, controlador, carta01, carta02,cartas, cards, entrada, acertos, quantidadeJogadas, tempo, segundo, tempoJogo, bloqueio
 
 tempo = document.querySelector(".relogio p")
 
@@ -10,6 +10,7 @@ function  iniciarJogo() {
   acertos= 0
   quantidadeJogadas = 0 
   segundos = 0;
+  bloqueio = false
 
   while ( entrada) {
     quantidadeCartas = parseInt(prompt("Qual a quantidade de Cartas? [min: 4 max:14]"))
@@ -19,6 +20,7 @@ function  iniciarJogo() {
         jogo(quantidadeCartas)
         entrada = false
         tempo.innerHTML = 0
+        tempoJogo = setInterval(relogio,1000)
       }
       
     } else entrada = true
@@ -38,12 +40,12 @@ function jogo(quantidadeCartas){
   cartas.forEach(element => {
       cards.innerHTML += `
 
-      <div class="card" onclick="virarCartas(this,'${element}')">
-        <div class=" face">
-        <img src="./imagens/front.png">
+      <div class="card" onclick="virarCartas(this,'${element}')" data-identifier="card">
+        <div class=" face" data-identifier="back-face">
+          <img src="./imagens/front.png">
         </div>
         <div class="back-face face">
-          <img src="./imagens/${element}.png">
+          <img src="./imagens/${element}.png" data-identifier="front-face">
         </div>
       </div>
   `
@@ -56,50 +58,74 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
- function virarCartas(carta,animal){
-    quantidadeJogadas++
-    carta.children[0].classList.add("front-face")
-    carta.children[1].classList.add("back-face1")
 
-  
-    if(controlador === false){
-      cardAnterior = animal
-      controlador = true;
-      carta01 = carta
-    }else{
-      cardAtual = animal
-      controlador = false;
-      carta02 = carta
-      validarJogada()
-    }
+ function virarCartas(carta,animal){
+  if(bloqueio === false){
+     
+      quantidadeJogadas++
+      carta.children[0].classList.add("front-face")
+      carta.children[1].classList.add("back-face1")
+
+    
+      if(controlador === false){
+        cardAnterior = animal
+        controlador = true;
+        carta01 = carta
+      }else{
+        cardAtual = animal
+        controlador = false;
+        carta02 = carta
+        bloqueio = true
+        validarJogada()
+      }
+  }
 
  }
 
  function validarJogada(){
 
      if( cardAnterior === cardAtual  ){
+       bloqueio = false
        acertos++
       if(acertos === quantidadeCartas/2) {
         clearInterval(tempoJogo)
         setTimeout(continuarJogo, 500)
       }
-       console.log("Voce esta no caminho Certo!")
      } else{
+       
        setTimeout(devirarCartas, 1000)
-       console.log("nao foi dessa vez!")
    }
    
  }
 
  function continuarJogo(){
+    let continuar = true
+    let jogar = ""
+
     alert(`"Você ganhou em ${quantidadeJogadas} jogadas e ${segundos} segundos!"`)
-    let jogar = prompt(` Deseja jogar novamente? N/S`)
-    // let jogar = prompt(` Parabéns você ganhou em ${quantidadeJogadas} jogadas e ${segundos} segundos. Deseja jogar novamente? N/S`)
-      if( jogar === 's'){
-        cards.innerHTML = ""
-        // tempo = setInterval(relogio,1000)
-        iniciarJogo()
+    
+    do{
+
+      jogar = prompt(` Deseja jogar novamente? N/S`)
+      jogar = jogar.toUpperCase()
+      console.log(jogar)
+
+      switch (jogar) {
+        case 'S':
+          cards.innerHTML = ""
+          iniciarJogo()
+          continuar = false
+          break;
+
+        case 'N':
+          alert(`Não se esqueça de voltar,viu!`)
+          continuar = false
+          break;
+      
       }
+
+    }while (continuar)
+      
  }
 
  function devirarCartas(){
@@ -109,6 +135,7 @@ function comparador() {
 
       carta02.children[0].classList.remove("front-face")
       carta02.children[1].classList.remove("back-face1")
+      bloqueio = false
 
  }
 
@@ -118,6 +145,6 @@ function comparador() {
  }
   
  iniciarJogo()
- const tempoJogo = setInterval(relogio,1000)
+ 
  
  
